81d90f6245b151216ed6d1b45dcee70592d1d2b1
swainasish <swainasish@yahoo.com>
2023-04-26 13:16:38 +0530

    docu_mod1

--- web_doc_database/spatial_prompt/docusaurus.config.js
+++ web_doc_database/spatial_prompt/docusaurus.config.js
@@ -13,7 +13,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   favicon: 'img/favicon.ico',
   organizationName: 'swainasish', // Usually your GitHub org/user name.
   projectName: 'SpatialPrompt', // Usually your repo name.
- // deploymentBranch : "gh-pages",
+  deploymentBranch : "gh-pages",
   
   presets: [
     [
